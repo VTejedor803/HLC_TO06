8247201acae8a2b341152f35b3616097fb435ea0
VTejedor803 <79998829+VTejedor803@users.noreply.github.com>
2022-05-27 16:21:30 +0200

    Update migracion.sql

--- data/migracion.sql
+++ data/migracion.sql
@@ -11,3 +11,13 @@ CREATE TABLE alumnos (
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE notas (
+  id int unsigned NOT NULL AUTO_INCREMENT,
+  id_alumno int unsigned NOT NULL,
+  referencia varchar(50) DEFAULT NULL,
+  nota double DEFAULT NULL,
+  PRIMARY KEY (id),
+  KEY fk_relacion (id_alumno),
+  CONSTRAINT fk_relacion FOREIGN KEY (id_alumno) REFERENCES alumnos (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
